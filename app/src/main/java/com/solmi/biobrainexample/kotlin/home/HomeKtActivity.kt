fd249c073ad98679a583214e03adacc786604ba3
mjh2550 <moomjh94@naver,com>
2022-04-23 17:59:17 +0900

    2022-04-23, Navigation UI Create

---
+++ app/src/main/java/com/solmi/biobrainexample/kotlin/home/HomeKtActivity.kt
@@ -0,0 +1,11 @@
+package com.solmi.biobrainexample.kotlin.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeKtActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_kt)
+    }
+}
