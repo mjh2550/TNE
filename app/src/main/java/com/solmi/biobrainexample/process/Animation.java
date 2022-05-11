5ebac1b4ac991b4686cca05e9cd794b3a7744e54
jhmoon <moomjh94@naver.com>
2022-05-11 15:51:42 +0900

    JAVA Source Delete

--- app/src/main/java/com/solmi/biobrainexample/process/Animation.java
+++
@@ -1,9 +0,0 @@
-package com.solmi.biobrainexample.process;
-
-public interface Animation {
-
-    public void onNextAnim();
-    public void onBackAnim();
-    public void onUpAnim();
-    public void onDownAnim();
-}
--- app/src/main/java/com/solmi/biobrainexample/process/BioStart.java
+++
@@ -1,9 +0,0 @@
-package com.solmi.biobrainexample.process;
-
-import android.bluetooth.BluetoothDevice;
-
-public interface BioStart {
-    //public void showNoti(BluetoothDevice device);
-    public void showNoti();
-    public void delNoti();
-}
