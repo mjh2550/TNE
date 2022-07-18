05a063929f993a43a12a0a0a460ba2777f368428
mjh2550 <moomjh94@naver.com>
2022-07-18 22:55:08 +0900

    smoothing window 구현

--- app/src/main/java/com/solmi/biobrainexample/common/BaseApplication.kt
+++ app/src/main/java/com/solmi/biobrainexample/common/BaseApplication.kt
@@ -5,13 +5,8 @@ import android.app.Application
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Color
-import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.ColorDrawable
-import android.icu.text.SimpleDateFormat
 import android.os.Build
-import android.widget.ImageView
-import android.widget.ProgressBar
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatDialog
 import androidx.core.content.ContextCompat
 import com.solmi.biobrainexample.R
@@ -86,5 +81,33 @@ class BaseApplication : Application(){
         return true
     }
 
+    /**
+     * 2022.07.18
+     * Moving Average (Int)
+     */
+
+    fun movingAverage(totalScore : Int,provideList : ArrayList<Int>) : ArrayList<Int>{
+        val m: Int = totalScore
+        val list: ArrayList<Int> = provideList
+        val n = list.size
+        val resultList = ArrayList<Int>()
+        
+        var sum = 0
+
+        for (i in 0 until m - 1) {
+            sum += list[i] //m-1까지의 부분sum
+        }
+
+        for (i in m - 1 until n) {
+            sum += list[i]
+            resultList.add(sum / m)
+            sum -= list[i - m + 1]
+        }
+
+        for(i in resultList){
+            println("average is $i")
+        }
+        return  resultList
+    }
 
 }
